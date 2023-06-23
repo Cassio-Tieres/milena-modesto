79dd4f136dfbcc0ac268fce48e7181f5192f0977
Cássio Tieres <tieresbomfim@gmail.com>
2023-06-23 20:49:16 -0300

    Configura bootstrap

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="Teste">
+        <div class="container">
+            <div class="col-lg-5">
+                Testando
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage'
+    }
+</script>
